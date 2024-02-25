45a7e3c50ae684d5404e88b14aa08fdae2658172
Darko Vukovic <vdarkobar@gmail.com>
2024-02-25 13:04:15 +0100

    Create pihole-install.sh

---
+++ pihole-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/expect -f
+
+set timeout 120
+
+# Spawn the installation command directly
+spawn /bin/bash -c "curl -sSL https://install.pi-hole.net | sudo bash"
+
+# Example handling of the installation script prompts
+#expect "Update" {
+#    send "yes\r"
+#}
+
+# Example handling of the installation script prompts
+expect "Existing Install Detected" {
+    send "yes\r"
+}
+
+# Continue with other prompts as necessary
+
+expect eof
